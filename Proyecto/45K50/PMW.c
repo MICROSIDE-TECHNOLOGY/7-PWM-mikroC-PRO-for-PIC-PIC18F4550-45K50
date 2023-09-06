dca15cb38015eed01331a5ca303743d1e7b7783b
vhperaltaj <victor.peralta@microside.com>
2023-09-06 16:39:47 -0600

    Fixed file encoding

--- Proyecto/45K50/PMW.c
+++ Proyecto/45K50/PMW.c
@@ -11,7 +11,7 @@ Driver Version    :  1.0
 ********************************************************************************/
 
 /*******************************************************************************
-Para usar el cï¿½digo con bootloader, configurar como lo indica MICROSIDE:
+Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 1) Tools> Options> Output> Output Settings> Marcar "Long HEX format"
 2) Project> Edit Project> Insertar 48.000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
